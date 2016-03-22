0d4f1f8fe4f55cf32804202da365ec554a029839
Xiaohan Gao <gaolegao007@gmail.com>
2016-03-22 09:56:50 -0500

    Main panel update
    Update main css

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <html>
 	<?php include "includes/header.html"; ?>
 	<body>
-		<?php include "includes/dropdown.html"; ?>
-		
+		<!-- <?php include "includes/dropdown.html"; ?> -->
+		<div class='main'>
+			wefwefewf<br />
+			wefwefewf<br />
+			wefwefewf<br />
+			wefwefewf<br />
+			wefwefewf<br />
+			var_dumpv<br />
+			var_dumpv<br />
+			var_dumpv<br />
+			<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+			var_dumpvv<br />
+			var_dumpvvv<br />
+			vvv<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+			wefwefewf<br />
+			wefwefewf<br />
+			wefwefewf<br />
+			var_dumpvvwefwefewf<br />
+		</div>
 	</body>
 </html>
