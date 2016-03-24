0b75194d9abbf83477fa820b46c81881697d5321
Xiaohan Gao <gaolegao007@gmail.com>
2016-03-24 09:35:02 -0500

    Site Structure #4
    - styles.css location change

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<div class='main'>
 			<p class='title'>HyperOChem</h1>
 			<hr>
-			<p class='subtitle'>Organic Chemistry</h2>
+			<p class='subtitle'>Organic Chemistry</p>
 			<p>Organic Chemistry is a subdiscipline of chemistry that focuses on the structure, properties, and reactions of organic compounds, or compounds containing carbon.
 			For undergraduates or even high school students, the amount and the complexity of the material is often overwhelming.
 			This site aims to help students grasp the concepts faster by providing resources such as summaries written by other students and links to helpful websites.</p>
