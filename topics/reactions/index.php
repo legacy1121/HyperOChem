c1193e9bcaeb8872a8568e11edef80fd5812e43d
Xiaohan Gao <gaolegao007@gmail.com>
2016-03-29 09:26:21 -0500

    Dropdown/Reactions

---
+++ topics/reactions/index.php
@@ -0,0 +1,15 @@
+<html>
+	<?php include "../../includes/header.html"; ?>
+		<title>Reactions</title>
+	</head>	
+	<body>
+		<div class='main'>
+			<p class='title'>Reactions</h1>
+			<hr>
+			<p class='subtitle'>Organic Chemistry</p>
+			<p>Stereochemistry is the branch of chemistry concerned with the three-dimensional arrangement of atoms and molecules and the effect of this on chemical reactions.</p>
+			<p class='subtitle'>Changelog</p>
+		</div>
+		<?php include "../../includes/dropdown.html"; ?>
+	</body>
+</html>
