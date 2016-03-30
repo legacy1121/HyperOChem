12e435d77ea8d4fa19eb6ff15baa4625d1dd8b6a
Xiaohan Gao <gaolegao007@gmail.com>
2016-03-30 09:50:39 -0500

    Nomenclature Update #1

--- topics/nomenclature/index.php
+++ topics/nomenclature/index.php
@@ -4,8 +4,33 @@
 	</head>	
 	<body>
 		<div class='main'>
-			<h2>Alkanes</h2>
-			<p>aoweijfowie</p>
+			<p class='title'>Nomenclature</p>
+			<hr>
+			<p class='subtitle'> Intro to Nomenclature Notes </p>
+			<p> Identify longest carbon chain or parent chain </p>
+			<img src="CarbonChain.png" alt="Carbon Chain" style="width:301px;height:134px;" />
+			<p>Name according to number of carbons in parent chain</p>
+			<img src="AlkaneChart.png" alt="Alkane Chart" style="width:676.5px;height:338.5px;" />
+			<p>Name according to the highest number of substituents when multiple possible parent chains</p>
+			<br />
+			<hr>
+			<p class='subtitle'> Common Substituent Names</p>
+			<img src="PropylButyl.png" alt="Propyl Butyl" style="width:507px;height:164px;" />
+			<p> Assign the substituents to have the lowest possible carbon attached </p>
+			<p> Order the naming alphabetically </p>
+			<p> Separate groups by dashes</p>
+			<br />
+			<hr>
+			<p class='subtitle'> Naming Alkenes </p>
+			<p> Alkenes contain a double bond </p>
+			<p> Use the same principles as alkanes but the suffix is -ene instead of -ane </p>
+			<p> The double bond should have the lowest possible carbon assigned </p>
+			<img src="Ene.png" alt="Ene" style="width:550px;height:132px;" />
+			<br />
+			<hr>
+			<p class='subtitle'> Naming Alkynes </p>
+			<p> Same principle as before but alkynes have a triple bond </p>
+			<img src="HCCH.png" alt="HCCH" style="width:250px;height:54px;" />
 		</div>
 		<?php include "../../includes/dropdown.html"; ?>
 	</body>
