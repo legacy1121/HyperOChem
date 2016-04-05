12f781c1c0205b6475a6fa1ef3717abd3a2937e0
darongqi <qi.darong@gmail.com>
2016-04-05 10:01:13 -0500

    dsfgsdfg

--- topics/stereochemistry/index.php
+++ topics/stereochemistry/index.php
@@ -10,15 +10,19 @@
 			<p>Stereochemistry is the branch of chemistry concerned with the three-dimensional arrangement of atoms and molecules and the effect of this on chemical reactions.</p>
 			<br/>
 			<hr>
+			<p class='subtitle'>Newman Projections</p>
+			<p>Newman projections are a useful tool to help assist with alkane stereochemistry. They allow for an easy method to view the molecule as a whole in order to analyze R and S configuration as well as they show different arrangements such as a “staggered” or “eclipsed” structure.</p>
+			<img src='newmanprojection.png' width = 20% height = 26%/>
+			<p>The above picture shows the difference in a staggered conformation and an eclipsed conformation. Typically the staggered conformation is preferred because the molecule is more spread out or evenly spaced and that reduces repulsion between the attachments on the first or front carbon and the attachments on the second or back carbon. The reason multiple configurations are possible is because alkanes are comprised of single bonds which are able to rotate.</p>
+			<br/>
+			<hr>
 			<p class='subtitle' id ='iso'>Isomers</p>
 			<p>
-				<ul>
-					<li>Isomers are two molecules with the same formula but different structure</li>
-					<li>Two different types of Isomers (Constitutional isomers and Stereoisomers)</li>
+			Isomers are two molecules with the same formula but different structure. There are two different types of Isomers (Constitutional isomers and Stereoisomers):
 					<li>Constitutional Isomers
 						<ul>
 							<li>Same molecular formula but different constitution (order of connectivity of atoms)</li>
-							<img src="constitutionalisomersdotjpeg.png" width = 30% height = 20% />
+							<img src="constitutionalisomersdotjpeg.png" width = 30% height = 17% />
 						</ul>
 					</li>
 					<li>Stereoisomers
@@ -39,6 +43,11 @@
 					<strong><a href="https://www.youtube.com/watch?v=kFD6hzLseVs">R and S Configuration</a></strong>
 				</ul>
 			</p>
+			<hr>
+			<p class='subtitle' id ='opa'>Optical Activity</p>
+			<p>If enantiomers have opposite configurations than they will rotate plane-polarized light in a different directions. Optically Active means a compound can rotate the plane in which light travels on. The equation for specific rotation is:</p>
+			<img src = "equation.png" width=5.5% height = 3.5%/>
+			<p>R and S configuration has no correlation to the direction in which the light travels. The direction is denoted by a +/-. The direction can only be determined through experiments and not predicted by structure or configuration.</p>
 		</div>
 		<?php include "../../includes/dropdown.html"; ?>
 	</body>
