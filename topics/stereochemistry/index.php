215c1298c9aa7c525a40f4b9d97752887f471d1e
darongqi <qi.darong@gmail.com>
2016-04-07 09:27:28 -0500

    removed some bullet points - put the links on the hr

--- topics/stereochemistry/index.php
+++ topics/stereochemistry/index.php
@@ -15,21 +15,15 @@
 			<img src='newmanprojection.png' width = 20% height = 26%/>
 			<p>The above picture shows the difference in a staggered conformation and an eclipsed conformation. Typically the staggered conformation is preferred because the molecule is more spread out or evenly spaced and that reduces repulsion between the attachments on the first or front carbon and the attachments on the second or back carbon. The reason multiple configurations are possible is because alkanes are comprised of single bonds which are able to rotate.</p>
 			<br/>
-			<hr>
-			<p class='subtitle' id ='iso'>Isomers</p>
+			<hr id ='iso'>
+			<p class='subtitle'>Isomers</p>
 			<p>
 			Isomers are two molecules with the same formula but different structure. There are two different types of Isomers (Constitutional isomers and Stereoisomers):
-					<li>Constitutional Isomers
-						<ul>
-							<li>Same molecular formula but different constitution (order of connectivity of atoms)</li>
+					<li>Constitutional Isomers have the same molecular formula but different constitution (order of connectivity of atoms)</li>
 							<img src="constitutionalisomersdotjpeg.png" width = 30% height = 17% />
-						</ul>
 					</li>
-					<li>Stereoisomers
-						<ul>
-							<li>Same molecular formula and constitution but molecules have different shape and arrangement</li>
+					<li>Stereoisomers have the same molecular formula and constitution but molecules have different shape and arrangement</li>
 							<img src="stereoisomersdotjpeg.png" width=30% height=25%/>
-						</ul>
 					</li>
 					<li>Understanding the Difference in R and S Configuration
 						<ol>
@@ -43,8 +37,8 @@
 					<strong><a href="https://www.youtube.com/watch?v=kFD6hzLseVs">R and S Configuration</a></strong>
 				</ul>
 			</p>
-			<hr>
-			<p class='subtitle' id ='opa'>Optical Activity</p>
+			<hr id ='opa'>
+			<p class='subtitle'>Optical Activity</p>
 			<p>If enantiomers have opposite configurations than they will rotate plane-polarized light in a different directions. Optically Active means a compound can rotate the plane in which light travels on. The equation for specific rotation is:</p>
 			<img src = "equation.png" width=5.5% height = 3.5%/>
 			<p>R and S configuration has no correlation to the direction in which the light travels. The direction is denoted by a +/-. The direction can only be determined through experiments and not predicted by structure or configuration.</p>
