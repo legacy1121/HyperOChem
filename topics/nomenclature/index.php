e65c190796ad6a6b1d087a7a9d8afa54b82654ba
darongqi <qi.darong@gmail.com>
2016-04-12 09:06:22 -0500

    cyclo

--- topics/nomenclature/index.php
+++ topics/nomenclature/index.php
@@ -14,28 +14,31 @@
 			<p>Substitutent groups are mini-chains that extend away from the parent chain.</p>
 			<p>When naming a chain with multiple substituent groups, name according to the highest number of substituent groups when there are multiple possible parent chains.</p>
 			<img src="substituent.png"/>
-			<hr id='cyc'>
-			<p class='subtitle'>Cycloalkanes</p>
-			<p>Cyclic compounds are molecules with a ring of carbons. When naming cyclic compounds, add the prefix <i>-cyclo</i> to the beginning of the name.</p>
 			<br />
-			<hr>
+			<hr id='csn'>
 			<p class='subtitle'> Common Substituent Names</p>
 			<img src="PropylButyl.png" alt="Propyl Butyl" style="width:507px;height:164px;" />
 			<p> Assign the substituents to have the lowest possible carbon attached </p>
 			<p> Order the naming alphabetically </p>
 			<p> Separate groups by dashes</p>
 			<br />
-			<hr>
+			<hr id='alkene'>
 			<p class='subtitle'> Naming Alkenes </p>
 			<p> Alkenes contain a double bond </p>
 			<p> Use the same principles as alkanes but the suffix is -ene instead of -ane </p>
 			<p> The double bond should have the lowest possible carbon assigned </p>
 			<img src="Ene.png" alt="Ene" style="width:550px;height:132px;" />
 			<br />
-			<hr>
+			<hr id='alkyne'>
 			<p class='subtitle'> Naming Alkynes </p>
 			<p> Same principle as before but alkynes have a triple bond </p>
 			<img src="HCCH.png" alt="HCCH" style="width:250px;height:54px;" />
+			<hr id='cyc'>
+			<p class='subtitle'>Cyclical compounds</p>
+			<p>Cyclic compounds are molecules with a ring of carbons. When naming cyclic compounds, add the prefix <i>cyclo-</i> to the beginning of the name.</p>
+			<p>When naming cycloalkanes with substituent groups, start numbering carbons to achieve the lowest possible numbers on the substituents.</p>
+			<p>When naming cycloalkenes and cycloalkynes, the bond always rests on the lowest possible number. Usually, this is accomplished by numbering carbons 1 and 2 as the two carbons next to the bond.</p>
+			<img src="cyclo.png"/>
 		</div>
 		<?php include "../../includes/dropdown.html"; ?>
 	</body>
--- topics/stereochemistry/index.php
+++ topics/stereochemistry/index.php
@@ -11,37 +11,37 @@
 			<br/>
 			<hr>
 			<p class='subtitle'>Newman Projections</p>
-			<p>Newman projections are a useful tool to help assist with alkane stereochemistry. They allow for an easy method to view the molecule as a whole in order to analyze R and S configuration as well as they show different arrangements such as a “staggered” or “eclipsed” structure.</p>
+			<p>Newman projections are a useful tool to help assist with alkane stereochemistry. They allow for an easy method to view the molecule as a whole in order to analyze R and S configuration as well as they show different arrangements such as a “staggered” or “ecppsed” structure.</p>
 			<img src='newmanprojection.png' width = 20% height = 26%/>
-			<p>The above picture shows the difference in a staggered conformation and an eclipsed conformation. Typically the staggered conformation is preferred because the molecule is more spread out or evenly spaced and that reduces repulsion between the attachments on the first or front carbon and the attachments on the second or back carbon. The reason multiple configurations are possible is because alkanes are comprised of single bonds which are able to rotate.</p>
+			<p>The above picture shows the difference in a staggered conformation and an ecppsed conformation. Typically the staggered conformation is preferred because the molecule is more spread out or evenly spaced and that reduces repulsion between the attachments on the first or front carbon and the attachments on the second or back carbon. The reason multiple configurations are possible is because alkanes are comprised of single bonds which are able to rotate.</p>
 			<br/>
 			<hr id ='iso'>
 			<p class='subtitle'>Isomers</p>
 			<p>
 			Isomers are two molecules with the same formula but different structure. There are two different types of Isomers (Constitutional isomers and Stereoisomers):
-					<li>Constitutional Isomers have the same molecular formula but different constitution (order of connectivity of atoms)</li>
+					<p>Constitutional Isomers have the same molecular formula but different constitution (order of connectivity of atoms).</p>
 							<img src="constitutionalisomersdotjpeg.png" width = 30% height = 17% />
-					</li>
-					<li>Stereoisomers have the same molecular formula and constitution but molecules have different shape and arrangement</li>
+					</p>
+					<p>Stereoisomers have the same molecular formula and constitution but their atoms have a different shape and arrangement.</p>
 							<img src="stereoisomersdotjpeg.png" width=30% height=25%/>
-					</li>
-					<li>Understanding the Difference in R and S Configuration
+					</p>
+					<p>Understanding the Difference in R and S Configuration
 						<ol>
-							<li>Compare the atomic numbers of the molecules. The highest atomic number receives the highest priority.</li>
-							<li>Arrange the molecule with the lowest priority facing away from you.</li>
-							<li>Find the order of the priorities and determine if number increase going clockwise or counterclockwise.</li>
-							<li>If clockwise, the configuration is R. If counterclockwise, the configuration is S.</li>
+							<p>Compare the atomic numbers of the atoms. The highest atomic number receives the highest priority.</p>
+							<p>Arrange the molecule with the lowest priority facing away from you.</p>
+							<p>From the other three atoms, find the order of the priorities and determine if the number increases going clockwise or counterclockwise.</p>
+							<p>If the arrangement is clockwise, the configuration is R. If counterclockwise, the configuration is S.</p>
 						</ol>
-					</li>
-					<li>For more visual help, watch the video.</li>
+					</p>
+					<p>For more visual help, watch the video.</p>
 					<strong><a href="https://www.youtube.com/watch?v=kFD6hzLseVs">R and S Configuration</a></strong>
 				</ul>
 			</p>
 			<hr id ='opa'>
 			<p class='subtitle'>Optical Activity</p>
-			<p>If enantiomers have opposite configurations than they will rotate plane-polarized light in a different directions. Optically Active means a compound can rotate the plane in which light travels on. The equation for specific rotation is:</p>
+			<p>If enantiomers have opposite configurations than they will rotate plane-polarized pght in a different directions. Optically Active means a compound can rotate the plane in which pght travels on. The equation for specific rotation is:</p>
 			<img src = "equation.png" width=5.5% height = 3.5%/>
-			<p>R and S configuration has no correlation to the direction in which the light travels. The direction is denoted by a +/-. The direction can only be determined through experiments and not predicted by structure or configuration.</p>
+			<p>R and S configuration has no correlation to the direction in which the pght travels. The direction is denoted by a +/-. The direction can only be determined through experiments and not predicted by structure or configuration.</p>
 		</div>
 		<?php include "../../includes/dropdown.html"; ?>
 	</body>
