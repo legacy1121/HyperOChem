49feabae854377e4b1f7c268f697697ff7d61d04
legacy1121 <nathankong11@yahoo.com>
2016-04-14 09:20:57 -0500

    Nomenclature Update
    Khai is lazy so I updated his stuff.

--- topics/nomenclature/index.php
+++ topics/nomenclature/index.php
@@ -4,41 +4,52 @@
 	</head>	
 	<body>
 		<div class='main'>
-			<p class='title'>Nomenclature</p>
-			<hr>
-			<p class='subtitle'> The IUPAC System - naming alkanes</p>
-			<p> First, identify the longest carbon chain or parent chain. For example, the alkane chain below contains 8 carbons in its longest chain. </p>
-			<img src="CarbonChain.png" alt="Carbon Chain" style="width:301px;height:134px;" />
-			<p>Then, you name the molecule according to number of carbons in parent chain. </p>
-			<img src="AlkaneChart.png" alt="Alkane Chart" style="width:676.5px;height:338.5px;" />
-			<p>Substitutent groups are mini-chains that extend away from the parent chain.</p>
-			<p>When naming a chain with multiple substituent groups, name according to the highest number of substituent groups when there are multiple possible parent chains.</p>
-			<img src="substituent.png"/>
-			<br />
-			<hr id='csn'>
-			<p class='subtitle'> Common Substituent Names</p>
-			<img src="PropylButyl.png" alt="Propyl Butyl" style="width:507px;height:164px;" />
-			<p> Assign the substituents to have the lowest possible carbon attached </p>
-			<p> Order the naming alphabetically </p>
-			<p> Separate groups by dashes</p>
-			<br />
-			<hr id='alkene'>
-			<p class='subtitle'> Naming Alkenes </p>
-			<p> Alkenes contain a double bond </p>
-			<p> Use the same principles as alkanes but the suffix is -ene instead of -ane </p>
-			<p> The double bond should have the lowest possible carbon assigned </p>
-			<img src="Ene.png" alt="Ene" style="width:550px;height:132px;" />
-			<br />
-			<hr id='alkyne'>
-			<p class='subtitle'> Naming Alkynes </p>
-			<p> Same principle as before but alkynes have a triple bond </p>
-			<img src="HCCH.png" alt="HCCH" style="width:250px;height:54px;" />
-			<hr id='cyc'>
-			<p class='subtitle'>Cyclical compounds</p>
-			<p>Cyclic compounds are molecules with a ring of carbons. When naming cyclic compounds, add the prefix <i>cyclo-</i> to the beginning of the name.</p>
-			<p>When naming cycloalkanes with substituent groups, start numbering carbons to achieve the lowest possible numbers on the substituents.</p>
-			<p>When naming cycloalkenes and cycloalkynes, the bond always rests on the lowest possible number. Usually, this is accomplished by numbering carbons 1 and 2 as the two carbons next to the bond.</p>
-			<img src="cyclo.png"/>
+			<!DOCTYPE html>
+<p class='subtitle'> Intro to Nomenclature Notes </p>
+<p> IUPAC System </p>
+<p> 1. Identify longest carbon chain or parent chain </p>
+<p> Ex. the chain below contains 8 carbons</p>
+<img src="CarbonChain.png" alt="Carbon Chain" style="width:301px;height:134px;" />
+
+<p>Name according to number of carbons in parent chain</p>
+
+<img src="AlkaneChart.png" alt="Alkane Chart" style="width:676.5px;height:338.5px;" />
+
+<p>Name according to the highest number of substituents when multiple possible parent chains</p>
+
+<img src="Subs.png" alt="Subs" style="width:520.3px;height:202px;" />
+
+<p> If the parent chain is cyclic (a ring of carbons), add the prefix, “cyclo” to the beginning of the parent name. (The parent name may NOT include carbons that are both in a ring and outside a ring)</p>
+<img src="Cyclo.png" alt="Cyclo" style="width:471px;height:111px;" />
+
+<p>2. Identify the substituents </p>
+
+<p>Count the number of carbons in each group branching off from the parent chain, and use the terms from the table to name the substituents. The terms are the same for substituents except substituents end in -yl instead of -ane</p>
+
+<p>Common substituent names</p>
+
+<img src="PropylButyl.png" alt="Propyl Butyl" style="width:507px;height:164px;" />
+
+<p> Assign the substituents to have the lowest possible carbon attached </p>
+<p> Order the naming alphabetically </p>
+<p> Separate groups by dashes</p>
+
+<p class='subtitle'> Naming Alkenes </p>
+<p> Alkenes contain a double bond </p>
+<p> Use the same principles as alkanes but the suffix is -ene instead of -ane </p>
+<p> The double bond should have the lowest possible carbon assigned </p>
+
+<img src="Ene.png" alt="Ene" style="width:550px;height:132px;" />
+
+<p class='subtitle'> Naming Alkynes </p>
+<p> Same principle as before but alkynes have a triple bond </p>
+
+<img src="HCCH.png" alt="HCCH" style="width:250px;height:54px;" />
+<p class='subtitle'> Bicyclo Nomenclature </p>
+<p>1. Count the total number of carbons in the entire molecule. This is the parent name (eg. ten carbons in the system would be decane)</p>
+<p>2. Count the number of carbons between the bridgeheads (see picture below), then place in brackets in descending order. (eg. [2,2,1])</p>
+<p>3. Place the word bicyclo at the beginning of the name</p>
+<img src="Bicyclo.png" alt="Bicyclo" style="width:163px;height:109px;" />
 		</div>
 		<?php include "../../includes/dropdown.html"; ?>
 	</body>
