0082bb8ca00eec4ba58cb4601dfbc3531ba550f8
darongqi <qi.darong@gmail.com>
2016-04-14 09:58:40 -0500

    Added one more part to stereochemistry

--- topics/stereochemistry/index.php
+++ topics/stereochemistry/index.php
@@ -42,6 +42,19 @@
 			<p>If enantiomers have opposite configurations than they will rotate plane-polarized pght in a different directions. Optically Active means a compound can rotate the plane in which pght travels on. The equation for specific rotation is:</p>
 			<img src = "equation.png" width=5.5% height = 3.5%/>
 			<p>R and S configuration has no correlation to the direction in which the pght travels. The direction is denoted by a +/-. The direction can only be determined through experiments and not predicted by structure or configuration.</p>
+			<br/>
+			<hr id = 'symchi'>
+			<p class='subtitle'>Symmetry and Chirality</p>
+			<p>A molecule with a singular chirality center can be considered chiral and have optical rotation. If a molecule has an even number of chiral centers than they may or may not be chiral.</p>
+			<p>Chiral vs Achiral- A molecule is achiral if it has a plane of symmetry. If a molecule has a plane of symmetry and has an even number of chiral centers than it is a meso compound. A molecule can also be achiral if it is superimposable of the mirror image.</p>
+			<br/>
+			<hr id = 'resolution'>
+			<p class='subtitle'>Resolution of Enantiomers</p>
+			<p>There are two ways to break a compound and to take advantage of the physical compounds:</p>
+			<ul>
+				<li>Distillation - separates compounds with differing boiling points</li>
+				<li>Recrystallization - separates compounds with differing solubilities</li>
+			</ul>
 		</div>
 		<?php include "../../includes/dropdown.html"; ?>
 	</body>
