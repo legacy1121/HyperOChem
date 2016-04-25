76f9dcdc029ffa627e3d8803995cc6891b54fad4
darongqi <qi.darong@gmail.com>
2016-04-25 09:19:25 -0500

    changelog

--- index.php
+++ index.php
@@ -11,6 +11,30 @@
 			For undergraduates or even high school students, the amount and the complexity of the material is often overwhelming.
 			This site aims to help students grasp the concepts faster by providing resources such as summaries written by other students and links to helpful websites.</p>
 			<p class='subtitle'>Changelog</p>
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li>Deleted</li>
+				<li>Update</li>
+				<li>Base</li>
+				<li>Changed .gitignore</li>
+				<li>Added .gitattributes & .gitignore files</li>
+			</ul>
 		</div>
 		<?php include "includes/dropdown.html";?>
 	</body>
