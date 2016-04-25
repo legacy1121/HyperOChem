943a62a26b5aa1f28bbc064939097ac7c5858677
darongqi <qi.darong@gmail.com>
2016-04-25 09:47:23 -0500

    finished changelog

--- index.php
+++ index.php
@@ -12,6 +12,49 @@
 			This site aims to help students grasp the concepts faster by providing resources such as summaries written by other students and links to helpful websites.</p>
 			<p class='subtitle'>Changelog</p>
 			<ul>
+				<li>2016:04:25 More Changelog Stuff</li>
+				<li>2016:04:25 Reactions #4</li>
+				<li>2016:04:25 Changelog update</li>
+				<li>2016:04:14 Added one more part to stereochemistry</li>
+				<li>2016:04:14 tiny dropdown edit</li>
+				<li>2016:04:14 added stuff to stereo</li>
+				<li>2016:04:14 Nomenclature Update</li>
+				<li>2016:04:12 cyclo</li>
+				<li>2016:04:11 Reactions #3</li>
+				<li>2016:04:11 Revert "Revert "Changed some nomenclature stuff""</li>
+				<li>2016:04:11 Revert "Changed some nomenclature stuff"</li>
+				<li>2016:04:11 Changed some nomenclature stuff</li>
+				<li>2016:04:07 asdf</li>
+				<li>2016:04:07 Reaction #2 Fix</li>
+				<li>2016:04:07 Reactions #2</li>
+				<li>2016:04:07 Spectroscopy #5</li>
+				<li>2016:04:07 removed some bullet points - put links on the hr</li>
+				<li>2016:04:06 dsfgsdfg</li>
+				<li>2016:04:05 Reactions #1</li>
+				<li>2016:04:01 Spectroscopy #4</li>
+				<li>2016:03:31 Spectroscopy #3</li>
+				<li>2016:03:30 Spectroscopy #2</li>
+				<li>2016:03:30 changed the stereochem file</li>
+				<li>2016:03:30 Nomenclature update #1</li>
+				<li>2016:03:30 Spectroscopy #1</li>
+				<li>2016:03:30 updated images for KL</li>
+				<li>2016:03:30 updated Stereochemistry</li>
+				<li>2016:03:29 resolving conflicts #3</li>
+				<li>2016:03:29 update spectro</li>
+				<li>2016:03:29 Icon/Logo #2</li>
+				<li>2016:03:29 Logo Update #1</li>
+				<li>2016:03:29 Dropdown #2</li>
+				<li>2016:03:29 CSS Update #8</li>
+				<li>2016:03:29 Header Fix</li>
+				<li>2016:03:29 Dropdown/Reactions</li>
+				<li>2016:03:29 Dropdown</li>
+				<li>2016:03:29 Merge Pull Request #6</li>
+				<li>2016:03:29 Changes Stereo</li>
+				<li>2016:03:24 CSS Update #6</li>
+				<li>2016:03:24 Chuang Update</li>
+				<li>2016:03:24 Merge Pull Request #5</li>
+				<li>2016:03:24 Dropdown update</li>
+				<li>2016:03:24 First commit</li>
 				<li>2016:03:24 Merge Pull Request #4</li>
 				<li>2016:03:24 Index Creation</li>
 				<li>2016:03:24 Site Structure #4</li>
