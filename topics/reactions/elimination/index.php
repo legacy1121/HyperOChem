78a425018ed002d78709e0b2aac13f366b40e2b7
Xiaohan Gao <gaolegao007@gmail.com>
2016-04-25 09:18:54 -0500

    Reactions #4

---
+++ topics/reactions/elimination/index.php
@@ -0,0 +1,41 @@
+<html>
+	<?php include "../../../includes/header.html"; ?>
+		<title>Elimination Reactions</title>
+	</head>	
+	<body>
+		<div class='main'>
+			<p class='title'>Elimination Reactions</h1>
+			<hr>
+			<p class='subtitle'>Introduction to Elimination Reactions</p>
+<i>Beta Elimination</i> or <i>1,2-Elimination</i> occurs when a proton in the beta (&beta;) position, meaning it is on the carbon adjacent to the alpha (&alpha;) carbon that the halogen is attached to, is removed together with the leaving group, causing a double bond to form between the two carbons. When the leaving group in an elimination reaction is a halide, the process is known as <i>dehydrohalogenation</i>, and when it is water, it is known as <i>dehydration</i>.
+
+
+
+
+
+<hr>
+
+
+<p class='subtitle'>Alkenes</p>
+Alkenes are organic molecules that contain at least one carbon to carbon double bond. Concerning elimination reactions, a <i>regioselective</i> reaction is one that can produce two or more constitutional isomers but still produces one major product. This mainly occurs when forming double bonds in the product and concerns cis and trans configurations. Sometimes, however, the cis and trans terms don’t apply like the following:
+
+In this way, when there are no common groups, the Z (<i>zussamen</i>) product takes the place of the cis and the E (<i>entegen</i>) product takes the place of the trans.
+<center><img src='alk.png'/></center>
+<hr>
+<p class='subtitle'>The E2 Mechanism</p>
+An E2 reaction is considered to be a bimolecular reaction because it uses two different reactants. The mechanism has to occur between a nucleophile and a substrate containing a hydrogen. First, the nucleophile “attacks” the positively charged hydrogen atom, breaking the bond between the hydrogen and the substrate.Then, the electrons from that bond shift and form a double bond. Because of overcrowding of electrons, there also has to be a loss of a leaving group. The resulting product is a nucleophile with a hydrogen atom attached, an alkene and a negatively charged leaving group.
+	When the reaction is <i>regioselective</i>, forming two products or more, the product that has the most substituted double-bond is known as the <i>Zaitsev product</i>, and the second most substituted is called the <i>Hofmann product</i>. Typically the Zaitsev will be produced in more abundance as it is a more stable molecule; however, the Hofmann product can be the major product in cases where the nucleophile is <i>sterically hindered</i> (the negative charge is attached to a very large molecule, so “attacks” in a less dense area on the substrate, producing a less substituted product).
+	When the reactant in an E2 reaction has two chiral carbons, the product produced will be stereospecific. This is because an elimination can only occur when the two carbons, hydrogen, and leaving group are <i>coplanar</i> meaning they must react while in the same plan, so that the &pi; bonds can be formed between the carbons. The two types of coplanar are syn and anti. <i>Syn-coplanar</i> occurs when the hydrogen and leaving group are aligned, while the <i>anti-coplanar</i> occurs when the two are opposite each other. This is illustrated as follows:
+	Because all six substituent groups are aligned with the ones across from them in the syn-coplanar orientation, the anti-coplanar orientation is much more likely to react and typically is the only reaction to produce a product. This leads to a single (stereospecific) alkene product.
+	When the hydrogen and leaving group are not exactly coplanar, they are still able to react. This does not change the reaction process, but is referred to as periplanar, so anti-coplanar occurs at a 180<sup>o</sup> angle while anti-periplanar occurs around 178<sup>o</sup> or 179<sup>o</sup>. In an E2 reaction, periplanar is the more generally used term.
+<hr>
+<p class='subtitle'>The E1 Mechanism</p>
+	The E1 reaction requires a single reactant (the substrate) that reacts in two steps with a base to form a double bond. For this reason, it can be identified as unimolecular. Like the S<sub>N</sub>1 reaction, the E1 becomes more reactive as the substrate is more substituted.
+	This is because the carbocations formed when the leaving group separates is more stable when it has more substituents.
+	The outcome of an E1 reaction is always regioselective. This is why an E2 reaction is preferable to making a definite product. The products of an E2 reaction can be manipulated by choosing a sterically hindered or non-sterically hindered base, but the E1 reaction forms multiple products of the same proportional amounts in either case. A major and minor product will be produced and the major product will contain a more substituted double bond.
+	The steps of an E1 reaction are as follows: (1, optional) Proton Transfer, (2) Loss of Leaving Group, (3, optional) Carbocation Rearrangement, and (4) Proton Transfer.
+		<center><img src='mec.png'/></center>
+		</div>
+		<?php include "../../../includes/dropdown.html"; ?>
+	</body>
+</html>
--- topics/reactions/index.php
+++ topics/reactions/index.php
@@ -8,19 +8,18 @@
 			<hr>
 			<p class='subtitle'>Mechanisms</p>
 			<a href='mechanisms'><p>Main page: Mechanisms</p></a>
-			<p>Mechanisms address </p>
+			<p>Mechanisms are the ways an reaction can happen. This section addresses nucleophiles, electrophiles, and the different kinds of reactions such as substitution and eliminations. </p>
 			<hr>
 			<p class='subtitle'>Substitution</p>
 			<a href='substitution'><p>Main page: Substitution Reactions</p></a>
-			<p>Something</p>
+			<p>A <i>Substitution Reaction</i> is a reaction in which one substituent group is exchanged for another. It can occur when a suitable electrophile is treated with a nucleophile. The electrophile in a substitution reaction is known as the <i>substrate</i>. Haloalkane, Alkyl Halide, and Organohalide are all names for a halogen (that can easily act as a leaving group) which is attached to an alkane; it is classified as primary, secondary, or tertiary based on number of substituents.</p>
 			<hr>
 			<p class='subtitle'>Elimination</p>
 			<a href='elimination'><p>Main page: Elimination Reactions</p></a>
-			<p>Lol</p>
+			<p><i>Beta Elimination</i> or <i>1,2-Elimination</i> occurs when a proton in the beta (&beta;) position, meaning it is on the carbon adjacent to the alpha (&alpha;) carbon that the halogen is attached to, is removed together with the leaving group, causing a double bond to form between the two carbons. When the leaving group in an elimination reaction is a halide, the process is known as <i>dehydrohalogenation</i>, and when it is water, it is known as <i>dehydration</i>.</p>
 			<hr>
 			<p class='subtitle'>Addition</p>
 			<a href='addition'><p>Main page: Addition Reactions</p></a>
-			<p>Hello</p>
 			<hr>
 		</div>
 		<?php include "../../includes/dropdown.html"; ?>
