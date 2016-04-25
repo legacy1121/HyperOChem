c388d72aac36c805d876c02563ba655ac040ec45
legacy1121 <nathankong11@yahoo.com>
2016-04-25 09:26:18 -0500

    About Update #1
    Added about page.

--- about/index.php
+++ about/index.php
@@ -1,10 +1,45 @@
 <html>
 	<?php include "../includes/header.html"; ?>
+	<link rel="stylesheet" href="styles.css" type="text/css"/>
 		<title>About</title>
 	</head>
 	<body>
 		<div class='main'>
-			awefew
+			<p class='title'>About</p>
+			<hr>
+			<p class='subtitle'>Created By:</p>
+			<br/>
+			<br/>
+			<p>
+				This website was created by Highland Park Highschool's Organic Chemistry Class of 2016. This class was instructed by Mr. Wenzen Chuang.
+			</p>
+			<img src="hplogo.png" id="hplogo"></img>
+			<br/>
+			<br/>
+			<p>
+				Instruction for this class was provided through Wiley's Organic Chemistry Second Edition Organic Chemistry textbook. This website served as the class's final project. The class divided the project into 5 different groups, in which each group worked on a separate topic within Organic Chemistry. The division for the subject with reference to the groups are provided below. 
+			</p>
+			<hr>
+			<p class='subtitle'>Website coding and structure</p>
+			<p>
+				Nathan Kong
+				Xiaohan Gao
+				Darong Qi
+				Khaileif Ngyuen-Hille
+			</p>
+			<p class='subtitle'>Nomenclature</p>
+			<p>
+				Will Douglass
+				Jake Taylor
+				Connor Berry
+			</p>
+			<p class='subtitle'>Spectroscopy</p>
+			<p>
+				Paco Lopez
+				Jordan Fox
+				Michael Zhao
+			</p>
+			
 		</div>
 		<?php include "../includes/dropdown.html"; ?>
 	</body>
---
+++ about/references/index.php
@@ -0,0 +1,11 @@
+<html>
+	<?php include "../includes/header.html"; ?>
+		<title>About</title>
+	</head>
+	<body>
+		<div class='main'>
+			awefew
+		</div>
+		<?php include "../includes/dropdown.html"; ?>
+	</body>
+</html>
